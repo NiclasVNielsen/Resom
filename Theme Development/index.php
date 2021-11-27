b6af8855ee1d16ab4256b3a070fce1c9c690fceb
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-11-27 22:04:19 +0100

    Ehhm yeah its been a little while

---
+++ Theme Development/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/lku6ell.css">
+    <title>Forside</title>
+</head>
+<body>
+    <header class="cyanTheme containerJustifySpaceBetween">
+        <div class="logo">
+            <img src="./assets/logo.svg" alt="Resom Logo">
+        </div>
+        <nav class="containerJustifySpaceBetween">
+            <a href="">Nav 1</a>
+            <a href="">Nav 2</a>
+            <a href="">Nav 3</a>
+            <a href="">Nav 4</a>
+            <a href="">Nav 5</a>
+        </nav>
+        <div class="login CTASmall">
+            <a href="">Login</a>
+        </div>
+    </header>
+    <section class="cyanTheme">
+        <div class="wrapper containerAlignCenter hero">
+            <div class="col6">
+                <h1>
+                    Bliv kosmetisk sygeplejerske
+                </h1>
+                <p>
+                    Vi tilbyder et spændende uddannelsesforløb for sygeplejerske som ønsker at videreuddanne sig til kosmetisk sygeplejerske. Vi garanterer sikkerhed og høj professionalisme gennem hele uddannelsen, hvor du vil have adgang til læringsmateriale som er både teoretisk relevant og praktisk varieret. 
+                </p>
+                <a href="" class="CTABig">
+                    Tilmeld
+                </a>
+            </div>
+            <div class="col5 push1">
+                <!-- IMG -->
+            </div>
+        </div>
+    </section>
+    <section class="whiteTheme centerTitle wrapper littleSpace">
+        <h2>
+            Kom godt igang
+        </h2>
+        <section class="containerJustifySpaceAround">
+            <div class="banner col3">
+                <div>
+                    
+                </div>
+                <h3>
+                    Kursuser
+                </h3>
+            </div>
+            <div class="banner col3">
+                <div>
+
+                </div>
+                <h3>
+                    Læringsfilm
+                </h3>
+            </div>
+            <div class="banner col3">
+                <div>
+
+                </div>
+                <h3>
+                    Finansiering
+                </h3>
+            </div>
+            <div class="banner col3">
+                <div>
+
+                </div>
+                <h3>
+                    Jobbank
+                </h3>
+            </div>
+        </section>
+    </section>
+    <section class="containerAlignCenter whiteTheme centerTitle gutterRight bodyText wrapper bigSpace">
+        <div class="col7 imageContainer">
+            <div class="PH">
+
+            </div>
+        </div>
+        <div class="col5">
+            <h3>
+                Om uddannelsen
+            </h3>
+            <p>
+                Som kosmetisk sygeplejerske vil du komme til at udfører kosmetiske 
+                behandlinger såsom filler og botox, og du blive en del af en branche som 
+                udvikler sig konstant.
+                <br><br>
+                Uddannelsen kræver at du har en uddannelse som sygeplejerske, det 
+                betyder også at du vil blive superviseret og registreret som medhjælp hos 
+                en læge. Efter endt uddannelse vil du have mulighed for at finde arbejde 
+                hos en allerede etableret klinik eller starte din helt egen klinik.
+            </p>
+        </div>
+    </section>
+</body>
+</html>
