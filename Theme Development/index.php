c1d2acbaa0014ecb13af931b35001ef7da313b23
NiclasNielsen <Niclasvnielsen@gmail.com>
2021-11-30 11:51:51 +0100

    initial responsive working

--- Theme Development/index.php
+++ Theme Development/index.php
@@ -10,39 +10,39 @@
 </head>
 <body>
     <header class="cyanTheme containerJustifySpaceBetween">
-        <div class="logo">
+        <div class="logo m-col8">
             <img src="./assets/logo.svg" alt="Resom Logo">
         </div>
-        <nav class="containerJustifySpaceBetween">
+        <nav class="containerJustifySpaceBetween m-disapear">
             <a href="">Nav 1</a>
             <a href="">Nav 2</a>
             <a href="">Nav 3</a>
             <a href="">Nav 4</a>
             <a href="">Nav 5</a>
         </nav>
-        <div class="login CTASmall">
+        <div class="login CTASmall m-disapear">
             <a href="">Login</a>
         </div>
     </header>
-    <section class="cyanTheme">
-        <div class="wrapper containerAlignCenter hero">
-            <div class="col6">
+    <section class="cyanTheme s-whiteTheme m-gutterBoth">
+        <div class="wrapper containerAlignCenter hero m-heightAuto s-flexReverse">
+            <div class="col6 m-col8">
                 <h1>
                     Bliv kosmetisk sygeplejerske
                 </h1>
-                <p>
+                <p class="m-center">
                     Vi tilbyder et spændende uddannelsesforløb for sygeplejerske som ønsker at videreuddanne sig til kosmetisk sygeplejerske. Vi garanterer sikkerhed og høj professionalisme gennem hele uddannelsen, hvor du vil have adgang til læringsmateriale som er både teoretisk relevant og praktisk varieret. 
                 </p>
-                <a href="" class="CTABig">
+                <a href="" class="CTABig m-box-center">
                     Tilmeld
                 </a>
             </div>
-            <div class="col5 push1 m-disapear">
+            <div class="col5 push1 m-disapear s-appear s-col4 s-pushNone">
                 <img src="./assets/circle.png" alt="Circle of despair">
             </div>
         </div>
     </section>
-    <section class="whiteTheme centerTitle wrapper littleSpace">
+    <section class="whiteTheme centerTitle wrapper littleSpace s-disapear">
         <h2>
             Kom godt igang
         </h2>
@@ -51,7 +51,7 @@
                 <div>
                     
                 </div>
-                <h3>
+                <h3 class="m-p-size">
                     Kursuser
                 </h3>
             </div>
@@ -59,7 +59,7 @@
                 <div>
 
                 </div>
-                <h3>
+                <h3 class="m-p-size">
                     Læringsfilm
                 </h3>
             </div>
@@ -67,7 +67,7 @@
                 <div>
 
                 </div>
-                <h3>
+                <h3 class="m-p-size">
                     Finansiering
                 </h3>
             </div>
@@ -75,19 +75,20 @@
                 <div>
 
                 </div>
-                <h3>
+                <h3 class="m-p-size">
                     Jobbank
                 </h3>
             </div>
         </section>
     </section>
-    <section class="containerAlignCenter whiteTheme centerTitle gutterRight bodyText wrapper bigSpace">
-        <div class="col7 imageContainer">
+    <!-- Gutter needs ajusting to work probably -->
+    <section class="containerAlignCenter whiteTheme centerTitle gutterRight bodyText wrapper bigSpace m-gutterBoth">
+        <div class="col7 imageContainer m-col8">
             <div class="PH">
 
             </div>
         </div>
-        <div class="col5">
+        <div class="col5 m-col8 m-space">
             <h3>
                 Om uddannelsen
             </h3>
