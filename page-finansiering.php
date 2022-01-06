ff562c88aae3d7268cc5f9829e2b3a524afa587b
NiclasNielsen <Niclasvnielsen@gmail.com>
2022-01-06 14:38:04 +0100

    oopsie doopsie jeg byttede rundt på 2 sider

--- page-finansiering.php
+++ page-finansiering.php
@@ -1,6 +1,16 @@
 <?php 
     get_header();
 ?>
+<div class="pageHeading cyanTheme">
+    <div class="wrapper container">
+        <figure class="col2 push1">
+            <img src="<?php echo get_bloginfo('template_url') ?>/assets/piggy.png">
+        </figure>
+        <h1 class="col8 push1">
+            Finiansiering
+        </h1>
+    </div>
+</div>
     
 <?php 
     get_footer();
--- page-job-bank.php
+++ page-job-bank.php
@@ -1,7 +1,28 @@
 <?php 
     get_header();
 ?>
-    
+<div class="pageHeading cyanTheme">
+    <div class="wrapper container">
+        <figure class="col2 push1">
+            <img src="<?php echo get_bloginfo('template_url') ?>/assets/bank.png">
+        </figure>
+        <h1 class="col8 push1">
+            Jobbank
+        </h1>
+    </div>
+</div>
+
+<a id="slUrl" href="https://www.sundhedplus.dk/sundhedplus-kredit/?c=S06864" target="_blank">
+<img id="sp-slBtn" src="https://sl.sundhedplus.dk/sl.gif?v=sp1-1.0&c=S06864" style="width:320px;height:auto;" alt="Ansøg om en Sundhed+ Kredit"/>
+</a>
+
+<p> 
+  Hos Resom Kosmetisk Uddannelse, tilbyder vi nu at du kan finansiere din uddannelse gennem Sundhed+.
+  Med Sundhed+, kan du dele betalingen ud over 24 - 60 måneder, til en fast lav rente.
+  Det er let og hurtigt og ansøge om en Sundhed+ Kredit - det tager blot et par minutter, og kræver blot NemID.
+</p>
+
+<script src="https://sl.sundhedplus.dk/sl.js?nc%22%3E</script>
 <?php 
     get_footer();
 ?>
