f215dd6f69ae8822f7976ca4c572435696c7fe8f
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-01-13 12:40:17 +0100

    end of day push.
    gotta end early today

--- page.php
+++ page.php
@@ -1,18 +1,36 @@
 <?php get_header(); ?>
+<style>
+  body{
+    color: #FFFFFF;
+    background: #96B3BA;
+  }
 
-<div class='woocommerce'>
-      <?php
-        if(have_posts()) {
-            while(have_posts()) : the_post();
-    ?>
-    
-    
-    <?php the_title(); ?>
-    <?php the_content(); ?>
-    
-    
-    <?php endwhile; } ?>
-        
-</div>
+  .woocommerce{
+    display: flex;
+  }
+
+  .wcmamtx_menu_left{
+    width: 4em;
+    height: 100vh;
+    background: #405D64;
+    overflow: hidden;
+  }
+</style>
+<section class='woocommerceContainer'>
+  <div class='woocommerce'>
+        <?php
+          if(have_posts()) {
+              while(have_posts()) : the_post();
+      ?>
+      
+      
+      <?php the_title(); ?>
+      <?php the_content(); ?>
+      
+      
+      <?php endwhile; } ?>
+          
+  </div>
+</section>
 
 <?php get_footer(); ?>
--- woocommerce.php
+++ woocommerce.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
 
-
-
-<div class="woocommerce">
- <?php woocommerce_content(); ?>
+<div class="woocommerce cyanTheme">
+    <?php woocommerce_content(); ?>
 </div>
 
 <?php get_footer(); ?>
