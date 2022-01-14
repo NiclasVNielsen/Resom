f6e90a1c71ab2b877b0d80ecfdcf015ebd72b7a1
NiclasNielsen <Niclasvnielsen@gmail.com>
2022-01-14 16:52:20 +0100

    end of day push

--- index.php
+++ index.php
@@ -1,71 +1,20 @@
 <?php 
     get_header();
 ?>
-            
-<section class="cyanTheme s-whiteTheme m-gutterBoth">
-    <div class="wrapper containerAlignCenter m-heightAuto s-flexReverse">
-        <div class="col6 m-col8 hero">
-            <?php the_field("bliv_kosmetisk_sygeplejerske")?>
-            <a href="" class="CTABig m-box-center">
-                Tilmeld
-            </a>
-        </div>
-        <div class="col5 push1 m-disapear s-appear s-col4 s-pushNone">
-            <img src="./assets/circle.png" alt="Circle of despair">
-        </div>
-    </div>
-</section>
-<section class="whiteTheme centerTitle wrapper littleSpace s-disapear">
-    <h2>
-        Kom godt igang
-    </h2>
-    <section class="containerJustifySpaceAround">
-        <div class="banner col3">
-            <div>
-                
-            </div>
-            <h3 class="m-p-size">
-                Kursuser
-            </h3>
-        </div>
-        <div class="banner col3">
-            <div>
 
-            </div>
-            <h3 class="m-p-size">
-                Læringsfilm
-            </h3>
-        </div>
-        <div class="banner col3">
-            <div>
+<div class="wrapper">
+    <?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post(); 
+                the_title( '<h1>', '</h1>' ); 
+                the_content();
+            endwhile; 
+        else: 
+            _e( 'Sorry, no pages matched your criteria.', 'textdomain' ); 
+        endif; 
+    ?>
+</div>
 
-            </div>
-            <h3 class="m-p-size">
-                Finansiering
-            </h3>
-        </div>
-        <div class="banner col3">
-            <div>
-
-            </div>
-            <h3 class="m-p-size">
-                Jobbank
-            </h3>
-        </div>
-    </section>
-</section>
-<!-- Gutter needs ajusting to work probably -->
-<section class="containerAlignCenter whiteTheme centerTitle gutterRight bodyText wrapper bigSpace m-gutterBoth zigZagPattern">
-    <div class="col7 imageContainer m-col8">
-        <div class="PH">
-
-        </div>
-    </div>
-    <div class="col5 m-col8 m-space">
-        <?php the_field("om_uddannelsen")?>
-    </div>
-</section>
-    
 <?php 
     get_footer();
 ?>
--- page.php
+++ page.php
@@ -1,36 +1,18 @@
 <?php get_header(); ?>
-<style>
-  body{
-    color: #FFFFFF;
-    background: #96B3BA;
-  }
 
-  .woocommerce{
-    display: flex;
-  }
-
-  .wcmamtx_menu_left{
-    width: 4em;
-    height: 100vh;
-    background: #405D64;
-    overflow: hidden;
-  }
-</style>
-<section class='woocommerceContainer'>
-  <div class='woocommerce'>
-        <?php
-          if(have_posts()) {
-              while(have_posts()) : the_post();
+<section class='woocommerceContainer cyanTheme'>
+  	<?php do_action( 'woocommerce_account_navigation' ); ?>
+  	<div style="width: 100%">
+      <?php
+        if(have_posts()) {
+            while(have_posts()) : the_post();
       ?>
-      
-      
-      <?php the_title(); ?>
+
       <?php the_content(); ?>
-      
-      
+
+
       <?php endwhile; } ?>
-          
-  </div>
+  	</div>
 </section>
 
 <?php get_footer(); ?>
